c2c1ea2be021e7a9b453eae188e81c6e49d83fd0
Emad Elmogy <emadelmogy619@gmail.com>
2018-01-07 18:53:25 +0200

    our first test

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# search_hotels_task
+# Search Hotels Task
+
+It is a Laravel 5.2 web application contains only one endpoint Api named `search_hotels` using PHP >= 7.1
+
+
+# Installation Steps
+
+- clone the project to your machine
+- run `php -r \"copy('.env.example', '.env');\"`
+- run `composer install`
+- run `php artisan key:generate`
+
+
+# Test The Api
+
+- run `php artisan serve`
+- test the api by using postman for example and go to this link `http://localhost:8000/api/v1/search_hotels?price_from=100&price_to=200`
+
+- you can search by name , city , price , and date
+
+- find our Test at tests/ExampleTest.php
+
+# To-do
+
+- Implement `travis` or `scrutinizer-ci` or any other CI tool for the project, Add the build status badges to our project README file
+
+- Use `codeclimate` or any other alternative to estimate the code quality and add it's badge to our project README file
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,13 +7,63 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic functional test example.
+     * A basic functional test search by name .
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testSearchByName()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+      $this->json('GET', '/api/v1/search_hotels', ['name' => 'Concorde Hotel'])
+               ->seeJson([
+                   'name' => 'Concorde Hotel',
+               ]);
+    }
+
+
+    /**
+     * A basic functional test search by city .
+     *
+     * @return void
+     */
+    public function testSearchByCity()
+    {
+      $this->json('GET', '/api/v1/search_hotels', ['city' => 'dubai'])
+               ->seeJson([
+                   'city' => 'dubai',
+               ]);
+    }
+
+
+    /**
+     * A basic functional test search by price .
+     *
+     * @return void
+     */
+    public function testSearchByPrice()
+    {
+      $this->json('GET', '/api/v1/search_hotels', ['price_from' => '100','price_to'=>'200'])
+            ->assertJsonStructure([
+                 'name',
+                 'price',
+                 'city',
+                 'availability',
+         ]);
+    }
+
+
+    /**
+     * A basic functional test search by date .
+     *
+     * @return void
+     */
+    public function testSearchByDate()
+    {
+      $this->json('GET', '/api/v1/search_hotels', ['date_from' => '10-10-2020','date_to'=>'15-10-2020'])
+            ->assertJsonStructure([
+                 'name',
+                 'price',
+                 'city',
+                 'availability',
+         ]);
     }
 }
